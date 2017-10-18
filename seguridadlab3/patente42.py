cd7ac69b70cba1bf957cb93617732bbd3f51a129
camilo <czepedah@uft.edu>
2017-10-18 01:19:38 -0300

    actualizacion de scripts

--- seguridadlab3/patente42.py
+++ seguridadlab3/patente42.py
@@ -6,8 +6,12 @@ letpatentes=[]
 numpatentes=[]
 
 #números de patentes posibles
-for z in range(10,100):
-    numpatentes.append(str(z))
+for z in range(0,100):
+    if(z<10):
+        numpatentes.append('0'+str(z))
+    else:
+        numpatentes.append(str(z))
+   
 
 #combinatorias de letras posibles
 for it in combinations_with_replacement(letra,2):
@@ -15,9 +19,12 @@ for it in combinations_with_replacement(letra,2):
         letpatentes.append(''.join(it)+''.join(item))
         
 
-#Patentes completas        
+#Patentes completas
+cont=0
 for element in product(letpatentes,numpatentes):
-    fichero.write(''.join(element)+"\n")
+    #fichero.write(''.join(element)+"\n")
+    cont+=1
+print(cont)
 
 print("fin")
-fichero.close()
+#fichero.close()
